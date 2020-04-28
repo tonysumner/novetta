a8b28df25286e33ba06d12af347507605a039525
tonysumner <tony.sumner@hotmail.com>
2020-04-28 16:56:32 -0400

    Add files via upload

---
+++ precache-manifest.1f53432cd2741f5b429eeb5f3797b3c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29978c067281aa7f2f0d264b9e8677bd",
+    "url": "/tonysumner/novetta/index.html"
+  },
+  {
+    "revision": "d505e2055e77094d8fb1",
+    "url": "/tonysumner/novetta/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f7400e89b9c5e1c3e672",
+    "url": "/tonysumner/novetta/static/js/2.2ae8715e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tonysumner/novetta/static/js/2.2ae8715e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d505e2055e77094d8fb1",
+    "url": "/tonysumner/novetta/static/js/main.6da88c65.chunk.js"
+  },
+  {
+    "revision": "3b05002296aedb6778f9",
+    "url": "/tonysumner/novetta/static/js/runtime-main.1b836d08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tonysumner/novetta/static/media/logo.5d5d9eef.svg"
+  }
+]);
